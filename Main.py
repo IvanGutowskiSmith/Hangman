b5cd011bf807c4fcfd8ace5e06f04f62d4a29aa9
IvanGutowskiSmith <81445359+IvanGutowskiSmith@users.noreply.github.com>
2023-05-11 19:09:44 +0100

    Lives counter added basic

--- Main.py
+++ Main.py
@@ -1,7 +1,11 @@
 wordToGuess = "theword"
+guessesRemaining = 9
 
 guessedLetter = input("Guess a letter\n")
 if wordToGuess.__contains__(guessedLetter):
     print("Letter " + guessedLetter + " is correct")
 else:
+    guessesRemaining = guessesRemaining -1
     print("Sorry letter " + guessedLetter + " is incorrect")
+    print("You have " + str(guessesRemaining) + " guesses remaining.")
+
