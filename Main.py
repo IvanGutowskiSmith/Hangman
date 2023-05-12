449e247a1a9dac30b81ab2cde4ea99a33bec9501
IvanGutowskiSmith <81445359+IvanGutowskiSmith@users.noreply.github.com>
2023-05-12 20:08:41 +0100

    Milestone 2, correct and incorrect guesses logged

--- Main.py
+++ Main.py
@@ -1,11 +1,49 @@
-wordToGuess = "theword"
-guessesRemaining = 9
+# Variables
+wordToGuess = "because"
+correctGuessedLetters = []
+incorrectGuessedLetters = []
+continueToGuess = True
 
-guessedLetter = input("Guess a letter\n")
-if wordToGuess.__contains__(guessedLetter):
-    print("Letter " + guessedLetter + " is correct")
-else:
-    guessesRemaining = guessesRemaining -1
-    print("Sorry letter " + guessedLetter + " is incorrect")
-    print("You have " + str(guessesRemaining) + " guesses remaining.")
+# Logic functions
 
+def printStatus():
+    for letter in wordToGuess:
+        if correctGuessedLetters.__contains__(letter):
+            print(letter,end="")
+        else:
+            print("_ ",end="")
+    
+    print("\n\nIncorrect guesses: "+str(incorrectGuessedLetters))
+
+
+def makeGuess():
+    guessedLetter = input()
+    if wordToGuess.__contains__(guessedLetter):
+        correctGuessedLetters.append(guessedLetter)
+    else:
+        incorrectGuessedLetters.append(guessedLetter)
+
+
+def continueGuessing():
+    moreLettersToGuess = False
+    for letter in wordToGuess:
+        if letter not in correctGuessedLetters:
+            moreLettersToGuess = True
+    
+    if moreLettersToGuess == True:
+        continueToGuess = True
+    else:
+        print("Well done, all letters guessed")
+        continueToGuess = False
+        
+
+# Main Loop
+
+print("Welcome to hangman, please guess a letter\n")
+printStatus()
+continueGuessing()
+
+while continueToGuess == True:
+    makeGuess()
+    printStatus()
+    continueGuessing()
