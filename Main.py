2c3afc6041e012285032f270d085f47fe16cc797
IvanGutowskiSmith <81445359+IvanGutowskiSmith@users.noreply.github.com>
2023-05-15 23:31:27 +0100

    Word complexity validation and level added

--- Main.py
+++ Main.py
@@ -1,14 +1,47 @@
 import requests
+import xml.etree.ElementTree as ET
 
-# Variables
-response_API = requests.get('https://random-word-api.herokuapp.com/word?length=5')
-wordToGuess = response_API.text.strip("[\"\"]")
+## Variables
+wordToGuess = ""
 correctGuessedLetters = []
 incorrectGuessedLetters = []
 continueToGuess = True
 
+
 # Logic functions
 
+def getRandomWord(desiredDificuilty):
+    withinRange = False
+    while withinRange == False:
+        # Obtain random word
+        response_API = requests.get('https://random-word-api.herokuapp.com/word?length=5')
+        wordToGuess = response_API.text.strip("[\"\"]")
+
+        readabilityScore = requests.post('https://datayze.com/callback/readability', data={'txt': wordToGuess})
+        root = ET.fromstring(readabilityScore.text)
+        fleschScore = float(root.find('.//scores').attrib['flesch'])
+        print("finding word....." + str(fleschScore))
+
+        # > 50 = easy
+        # < 50 = hard
+        withinRange = False
+        match desiredDificuilty:
+            case "1":
+                if fleschScore >= 50:
+                    withinRange = True
+                    break
+            case "2":
+                if fleschScore < 50:
+                    withinRange = True
+                    break
+            case _:
+                print("Enter value between 1 and 2")
+                break
+        
+    return wordToGuess
+
+
+    
 def printStatus():
     for letter in wordToGuess:
         if correctGuessedLetters.__contains__(letter):
@@ -38,11 +71,14 @@ def continueGuessing():
     else:
         print("Well done, all letters guessed")
         continueToGuess = False
-        
+       
 
 # Main Loop
 
-print("Welcome to hangman, please guess a letter\n")
+print("Welcome to hangman, please type your dificuilty level: Easy(1) - Hard (2)\n")
+wordToGuess = getRandomWord(input())
+print(wordToGuess)
+
 printStatus()
 continueGuessing()
 
@@ -50,3 +86,5 @@ while continueToGuess == True:
     makeGuess()
     printStatus()
     continueGuessing()
+
+exit()
